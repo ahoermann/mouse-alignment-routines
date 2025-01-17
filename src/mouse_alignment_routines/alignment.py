b314b102d36bf217dab74e8d26e8a1759d75bd9e
ahoerman <anja.hoermann@bam.de>
2025-01-17 19:12:53 +0100

    add store_location to all alignment routines

--- src/mouse_alignment_routines/alignment.py
+++ src/mouse_alignment_routines/alignment.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import numpy as np
+import epics
 from . import transmission_models as tm
 from logbook2mouse.scan import scan
 from logbook2mouse.measure_config import (
     move_motor, move_to_sampleposition
     )
 
-def center_pitch(experiment, limits, npoints, sampleposition):
+def center_pitch(experiment, limits, npoints, sampleposition, store_location):
     "scan of pitch angle with fit, ensuring that the motor is always scanned in the negative direction"
     if limits[0] < limits[1]:
         scan("pitchgi", limits[0], limits[1], npoints, 1,
-             experiment, sampleposition)
+             experiment, sampleposition, store_location)
     else:
         scan("pitchgi", limits[1], limits[0], npoints, 1,
-             experiment, sampleposition)
+             experiment, sampleposition, store_location)
     data = pd.read_csv("current_scan.csv", delimiter = ",",
                        )
     motorname = data.columns[0].split(":")[-1]
@@ -26,9 +27,9 @@ def center_pitch(experiment, limits, npoints, sampleposition):
     tm.pitch_params["x0"].set(value = center, min = -1,  max = 1)
     return center, beam_offset
 
-def zheavy_center(experiment, limits, npoints, sampleposition):
+def zheavy_center(experiment, limits, npoints, sampleposition, store_location):
     scan("zheavy", limits[0], limits[1], npoints, 1,
-         experiment, sampleposition)
+         experiment, sampleposition, store_location)
     data = pd.read_csv("current_scan.csv", delimiter = ",",
                        )
     motorname = data.columns[0].split(":")[-1]
@@ -43,9 +44,9 @@ def zheavy_center(experiment, limits, npoints, sampleposition):
     tm.z_params["sigma"].set(value = sigma, min = 0, max = 1)
     return center, sigma 
 
-def horizontal_center(experiment, limits, npoints, sampleposition):
+def horizontal_center(experiment, limits, npoints, sampleposition, store_location):
     scan("ysam", limits[0], limits[1], npoints, 1,
-                      experiment, sampleposition)
+                      experiment, sampleposition, store_location)
     data = pd.read_csv("current_scan.csv", delimiter = ",",
                        )
     motorname = data.columns[0].split(":")[-1]
@@ -63,7 +64,7 @@ def pitch_limit(sigma_beam, halfsample):
     # \(\pm 2 \frac{\sigma_{\mathrm{beam}}}{\tfrac{1}{2} l_{\mathrm{sample}}}\)
     return np.rad2deg(2 * sigma_beam/halfsample)
 
-def pitch_align(experiment, start_z, start_pitch, sigma_beam, halfsample=15, sampleposition={"ysam.blank": -66}):
+def pitch_align(experiment, start_z, start_pitch, sigma_beam, halfsample=15, sampleposition={"ysam.blank": -66}, store_location):
     center, sigma = start_z, sigma_beam
     move_motor("zheavy", center)
     move_motor("pitchgi", start_pitch)
@@ -71,11 +72,11 @@ def pitch_align(experiment, start_z, start_pitch, sigma_beam, halfsample=15, sam
     # not sure how the logic of this works
     pitch_center = start_pitch
     new_center, new_sigma = zheavy_center(experiment, (-2*sigma_beam, +2*sigma_beam), 31,
-                                          sampleposition)
+                                          sampleposition, store_location)
     move_motor("zheavy", new_center) 
     pitch_delta = pitch_limit(new_sigma, halfsample)
     new_pitch_center, new_beam_offset = center_pitch(experiment, (-pitch_delta, +pitch_delta), 31,
-                                                     sampleposition)
+                                                     sampleposition, store_location)
     move_motor("pitchgi", new_pitch_center)
     move_motor("zheavy", new_center+new_beam_offset)
     # with stopping condition but not adaptive number of points 
@@ -84,11 +85,11 @@ def pitch_align(experiment, start_z, start_pitch, sigma_beam, halfsample=15, sam
         pitch_center = new_pitch_center
         beam_offset = new_beam_offset
         new_center, new_sigma = zheavy_center(experiment, (-2*sigma_beam, +2*sigma_beam), 31,
-                                              sampleposition)
+                                              sampleposition, store_location)
         move_motor("zheavy", new_center) 
         pitch_delta = pitch_limit(new_sigma, halfsample)
         new_pitch_center, new_beam_offset = center_pitch(experiment, (-pitch_delta, +pitch_delta), 31,
-                                                         sampleposition)
+                                                         sampleposition, store_location)
         move_motor("pitchgi", new_pitch_center)
         move_motor("zheavy", new_center+new_beam_offset)
         logging.info(f"preliminary horizontal position pitch: {new_pitch_center}°")
@@ -97,20 +98,20 @@ def pitch_align(experiment, start_z, start_pitch, sigma_beam, halfsample=15, sam
         
     
 def roll_align(experiment, y_center, sigma_beam, rolloffset, centerofrotation = 30,
-               sampleposition={"ysam.blank": -66}):
+               sampleposition={"ysam.blank": -66}, store_location):
     y_neg = y_center - rolloffset
     move_motor("ysam", y_neg)
     neg_center, sigma = zheavy_center(experiment, (-2*sigma_beam, +2*sigma_beam), 31,
-                                      sampleposition)
+                                      sampleposition, store_location)
     y_pos = y_center + rolloffset
     move_motor("ysam", y_pos)
     pos_center, sigma = zheavy_center(experiment, (-2*sigma_beam, +2*sigma_beam), 31,
-                                      sampleposition)
+                                      sampleposition, store_location)
     print("positive edge:", pos_center)
     print("negative edge:", neg_center)
     rollangle = np.rad2deg(np.arctan((pos_center - neg_center)/(2 * rolloffset)))
     print("roll angle:", rollangle)
-    oldroll = scanmanager.get_pos("rollgi")
+    oldroll = epics.caget("mc0:rollgi")
     move_motor("rollgi", oldroll + rollangle) # to do: check sense of roll stage
     move_motor("ysam", y_center)
     if np.sign(y_pos - centerofrotation) == np.sign(y_neg - centerofrotation):
